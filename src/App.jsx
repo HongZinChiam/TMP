0dacaddd99454937822b3793ca7b854b16cd557f
Ren <ren12death@gmail.com>
2024-09-03 06:18:13 +0800

    Added NavBar and ProductBacklog to App
    Added NavBar.jsx and ProductBacklog.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Header from './Header.jsx'
+import NavBar from './NavBar.jsx'
+import ProductBacklog from './ProductBacklog.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <div class="grid">
+        <NavBar />
+        <ProductBacklog />
+      </div>
     </>
   )
 }
