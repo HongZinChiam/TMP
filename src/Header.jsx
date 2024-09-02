e8421658c1db43e01082136e181b16e739a156b5
Ren <ren12death@gmail.com>
2024-09-03 05:43:53 +0800

    Added Header.jsx
    Added a Header component

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <h1>TaskManagerPro</h1>
+            <i class="fa-solid fa-user user-icon"></i>
+        </header>
+    )
+}
+
+export default Header
