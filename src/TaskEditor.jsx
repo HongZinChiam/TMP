ce5b0796a647f7c0d35f88937e0894fcbfc99ccb
Ren <ren12death@gmail.com>
2024-09-04 01:37:55 +0800

    Changes to TaskEditor.jsx
    Added input fields, Assign, Task Status, Task Stages

--- src/TaskEditor.jsx
+++ src/TaskEditor.jsx
@@ -10,6 +10,10 @@ function TaskEditor() {
     const [priorityRating, setPriorityRating] = useState('');
     const [tags, setTags] = useState('');
 
+    const [assign, setAssign] = useState('');
+    const [taskStatus, setTaskStatus] = useState('');
+    const [taskStages, setTaskStages] = useState('');
+
     return (
         <form>
             <section>
@@ -84,6 +88,50 @@ function TaskEditor() {
                     </select>
                 </div>
             </section>
+
+            <section class="column">
+                <div>
+                    <label for="assign">Assign</label>
+                    <select
+                        id="assign"
+                        className="blue input column-input"
+                        required
+                        value={assign}
+                        onChange={e => setAssign(e.target.value)}
+                    >
+                    </select>
+                </div>
+                <div>
+                    <label for="taskStatus">Task Status</label>
+                    <select
+                        id="taskStatus"
+                        className="blue input column-input"
+                        required
+                        value={taskStatus}
+                        onChange={e => setTaskStatus(e.target.value)}
+                    >
+                        <option value="notStarted">Not Started</option>
+                        <option value="active">Active</option>
+                        <option value="completed">Completed</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="taskStages">Task Stages</label>
+                    <select
+                        id="taskStages"
+                        className="blue input column-input"
+                        required
+                        value={taskStages}
+                        onChange={e => setTaskStages(e.target.value)}
+                    >
+                        <option value="planning">Planning</option>
+                        <option value="development">Development</option>
+                        <option value="integration">Integration</option>
+                        <option value="testing">Testing</option>
+                    </select>
+                </div>
+            </section>
+
             <section>
                 <button className="save">Save</button>
             </section>
