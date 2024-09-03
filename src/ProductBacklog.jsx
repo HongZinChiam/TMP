a346c05754e61d803dac58e7a0fbbf3d06fb1dd3
Ren <ren12death@gmail.com>
2024-09-03 08:09:10 +0800

    Changes to ProductBacklog.jsx
    Added a button to toggle between ProductBacklog and TaskEditor

--- src/ProductBacklog.jsx
+++ src/ProductBacklog.jsx
@@ -1,16 +1,31 @@
 import "./ProductBacklog.css"
 import TaskCard from "./TaskCard"
+import { useState } from "react"
+import TaskEditor from "./TaskEditor"
 
 function ProductBacklog() {
+
+    const [showEditor, setShowEditor] = useState(false)
+    
     return (
         <div class="container">
-            <section class="backlog">
-                <h2>Product Backlog</h2>
-                <div class="tasks">
-                    <TaskCard />
-                    <TaskCard />
-                </div>
-            </section>
+            {
+                !showEditor
+                ? (
+                    <section class="backlog">
+                        <h2>Product Backlog</h2>
+                        <div class="tasks">
+                            <TaskCard />
+                            <TaskCard />
+                        </div>
+                        <button class="add" onClick={() => setShowEditor(prevShowEditor => !prevShowEditor)}>
+                            <i class="fa-solid fa-plus"></i>
+                            <p>Add</p>
+                        </button>
+                    </section>
+                )
+                : <TaskEditor />
+            }
         </div>
     )
 }
